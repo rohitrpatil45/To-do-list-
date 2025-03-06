ea25483b6f3a8bd638de6612dd8f6c32772e5ba9
Rohit Patil <rohitrajupatil12@gmail.com>
2025-03-06 16:25:25 +0530

    add the js code little bit

---
+++ script.js
@@ -0,0 +1,9 @@
+const AddBtn = document.querySelector(".add-btn")
+
+const Input = document.querySelector(".add-btn")
+
+const InputRadio = document.querySelector(".inputRadio")
+
+const task = document.querySelector(".task-bar")
+
+const remove = document.querySelector(".task-bar")
