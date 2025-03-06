4df30068d528e85a586efd9d49b5af593b4d5a71
Rohit Patil <rohitrajupatil12@gmail.com>
2025-03-06 16:38:52 +0530

    add the eventliner in js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const AddBtn = document.querySelector(".add-btn")
 
-const Input = document.querySelector(".add-btn")
+const Input = document.querySelector(".input-tag")
 
 const InputRadio = document.querySelector(".inputRadio")
 
 const task = document.querySelector(".task-bar")
 
 const remove = document.querySelector(".task-bar")
+
+Input.addEventListener('input' , (e) =>{
+
+    const lacaldata = localStorage.setItem("task" , e.target.value)
+    task.innerHTML = lacaldata ;
+    console.log(e.target.value)
+})
+
+AddBtn.addEventListener('click' , () => {
+    console.log("form the addbtn" , lacaldata)
+
+
+})
